8d79b4ffa3d06731f05bad94f3fda098a7be1e34
JenyaFomin <jenya.stasik@gmail.com>
2024-03-08 17:05:09 -0500

    feat: initialise the project with some css, api and react

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+        domains: ['theme.dsngrid.com'],
+        formats: ['image/webp']
+    },
+    // i18n: {
+    //     locales: ["en"],
+    //     defaultLocale: "en",
+    // },
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
+    // pageExtensions: ["route.ts"]
+
+}
+
+module.exports = nextConfig
---
+++ pages/api/cars/route.ts
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth/next"
+import { options } from "../auth/[...nextauth]"
+
+export async function GET (req, res) {
+  console.log("GET CARS")
+  return res.send({
+    content:
+      "This is protected content. You can access this content because you are signed in.",
+  })
+    const session = await getServerSession(req, res, options)
+  
+    if (session) {
+      res.send({
+        content:
+          "This is protected content. You can access this content because you are signed in.",
+      })
+    } else {
+      res.send({
+        error: "You must be signed in to view the protected content on this page.",
+      })
+    }
+  }
